4779cd28bb5fd46d2cad0e070c8055aea95c2dcd
xungeng <xungeng@gmail.com>
2026-05-30 11:35:19 +0800

    add.insert.copy

--- config/bindings.lua
+++ config/bindings.lua
@@ -56,6 +56,8 @@ local keys = {
    -- copy/paste --
    { key = 'c',          mods = 'CTRL|SHIFT',  action = act.CopyTo('Clipboard') },
    { key = 'v',          mods = 'CTRL|SHIFT',  action = act.PasteFrom('Clipboard') },
+   { key = 'Insert',     mods = 'CTRL',     action = act.CopyTo('Clipboard') },
+   { key = 'Insert',     mods = 'SHIFT',    action = act.PasteFrom('Clipboard') },
    { key = 'c',          mods = mod.SUPER,  action = act.CopyTo('Clipboard') },
    { key = 'v',          mods = mod.SUPER,  action = act.PasteFrom('Clipboard') },
    { key = 'k',          mods = mod.SUPER,  action = act.ClearScrollback('ScrollbackAndViewport') },
